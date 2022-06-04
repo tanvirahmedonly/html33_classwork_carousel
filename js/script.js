2020d21111ce779af35fc70c250db4245b853be4
Tanvir Ahmed <tanvirahmedonly@gmail.com>
2022-06-04 23:19:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// live alert
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Wow, you triggered this successfully appended alert message!', 'success')
+  })
+}
